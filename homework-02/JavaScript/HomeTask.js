26bf32833b4e9fc3d29b4cd18336476624757677
Kate <katarinamadness@mail.ru>
2018-08-27 12:28:54 +0300

    Done

---
+++ homework-02/JavaScript/HomeTask.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let userInput;
+
+const numbers = [];
+
+do {
+    userInput= prompt('Введите число', '');
+    if(!isNaN(Number(userInput))) {
+        numbers.push(Number(userInput));
+    } else {
+        alert('Было введено не число, попробуйте еще раз');
+    }
+
+} while (userInput !== null);
+
+let total = 0;
+for(let item of numbers){
+    total = total + item;
+}
+alert(`Общая сумма чисел равна ${total}`);
