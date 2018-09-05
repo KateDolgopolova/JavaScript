1ca7ba94427ed3684df97c8a6e1db712876fdec1
Kate <katarinamadness@mail.ru>
2018-09-05 22:16:12 +0300

    Done

---
+++ homework-03/js/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+const login = "testlogin";
+//const login = prompt("Введите логин", );
+
+function isLoginValid(login){
+    return login.length >= 4 && login.length <= 16;
+}
+
+function isLoginUnique(logins, login){
+    for (let loginItem of logins){
+        if (loginItem === login) {
+            return false
+        }
+    }
+    return true
+}
+
+
+
+function addLogin(logins, login){
+   if (isLoginValid(login)){
+       if(isLoginUnique(logins, login)){
+           logins.push(login);
+           console.log('Логин успешно добавлен!');
+       } else {
+           console.log('Такой логин уже используется!');
+       }
+
+   } else{
+       console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+   }
+}
+
+addLogin(logins, login);
+console.log(logins);
